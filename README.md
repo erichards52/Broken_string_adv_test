33d8af6a9c183a212445f7045a8ac07f4e5b451c
erichards52 <edward.richards5252@gmail.com>
2023-11-21 11:44:59 +0000

    Updating analysis and README

--- README.md
+++ README.md
@@ -1,18 +1,44 @@
 # Advanced Coding Test: Broken String Biosciences
+### Answers to questions posed from the original GitLab repo can be found at the bottom of this REAMDE
 
 ## Pipeline explanation:
 This pipeline performs 4 steps:
 1. Filters the provided break bedfiles for a mapQ of >= 30
+
 2. Intersects each sample break bedfile with the AsiSI site bed file
+
 3. Sum(s) and normalise(s) the counts provided by bedtools
    * Sum the number of breaks per sample (provided by bedtools)
    * Normalise the number of breaks per sample (Sum of breaks/(Total Breaks/1000))
+
 4. Collect normalised number of AsiSI breaks
 
 ## Usage guide: 
 1. In order to run this you can use the pipeline by making sure you have all requirements (will make this/Dockerfile)
-1. Clone the repository ```git clone https://github.com/erichards52/Broken_string_adv_test.git```
-2. Pipeline can be run using: 
+
+2. Clone the repository ```git clone https://github.com/erichards52/Broken_string_adv_test.git```
+
+3. Pipeline can be run using: 
 ```nextflow run main.nf --bed_file data/breaks/ --bed_file_intersect ./chr21_AsiSI_sites.t2t.bed```
    * Pipeline uses the Nextflow workflow manager. Make sure you haven't moved any of the files present in the base level directory as all are required to be present. 
    * If you wish to move the location of the break bedfiles you can do this by specifying with the ```--bed_file <$PATH_TO_BREAK_BEDFILES>``` or if you wish to move the location of the AsiSI site bed file, this can be done via ```--bed_file_intersect <$PATH_TO_AsiSI_SITE_BEDFILE>```
+
+4. Output files as well as pipeline meta information can be found in the ```results_<$yyyy-MM-dd_HH-mm-ss>``` directory, which contains individual directories (named appopriately) containing outputs for each individual parallel process as well as a directory named ```pipeline_meta``` which contains a DAG, trace, as well as two viewable HTML reports (timeline and report)
+
+## N.B./Additional Info
+* (Python) Scripts used within processes can be found in the ```/bin``` directory
+
+* All analysis was done from within the ```/analysis``` directory (```analysis_script.py```) after having copied the final output of the pipeline (```combined_out.txt```) into the ```/analysis``` directory
+
+* Whilst timing decorators are not found within the pipeline inside individual processes, the trace files as well as HTMLs produced via Nextflow can be used to view an extensive number of traits from each parallel process' undertaken by the pipeline during each run
+
+## Answers to original repo questions:
+1. Which of the samples are likely to be controls or treated?
+   * Samples 1-8 are likely to be controls and samples 9,10,12,15 and 16 are likely to be the treated
+
+2. Are there any you are uncertain of?
+   * I am uncertain as to whether samples 11,13 and 14 are treated or a control as they exhibit the greatest distance from the cluster's centroid
+
+3. Can you explain the samples in the uncertain group?
+   * There are a number of reasons for each sample potentially belonging to the control group or the treated group:
+      1.  
--- analysis/analysis_script.py
+++ analysis/analysis_script.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-# Read in pipeline output into pandas dataframe
+# Read in pipeline output that has been copied into current directory into pandas dataframe
 pipe_out_df = pd.read_csv("combined_out.txt", sep="\t",header=None)
 
 # Take just the normalised counts
@@ -13,7 +13,7 @@ num_list = pipe_out_df[1].tolist()
 # Reshape the data to KMeans format
 samples_array = np.array(num_list).reshape(-1, 1)
 
-# initialise wcss array to use "Elbow Method" to determine best number of clusters to use
+# initialise wcss list to use "Elbow Method" to determine best number of clusters to use
 # we already know it's control vs. experiment, but for the sake of clarity/understanding
 wcss = []  
 
@@ -23,9 +23,9 @@ max_clusters = 6
 # Iterate over number of clusters
 for i in range(1, max_clusters + 1):
 
-    #Generate kmeans objects with increasing number of clusters until final iteration 
-    # choose a random seed from which to start from (I like 52)
-    # and choose how many iterations of starting with different initial centroid seeds
+    # Generate kmeans objects with increasing number of clusters until final iteration 
+    # choose a random seed from which to start from 
+    # and choose how many iterations of starting with differing initial centroid location(s)
     kmeans = KMeans(n_clusters=i, random_state=52, n_init=10)
     kmeans.fit(samples_array)
 
@@ -47,8 +47,8 @@ plt.savefig('elbow_method.png', bbox_inches='tight')
 cluster_num = 2  
 
 # Fit KMeans model to the sample values with our chosen criteria (2 clusters)
-# choose a random seed from which to start from (I like 52)
-# and choose how many iterations of starting with different initial centroid seeds
+# choose a random seed from which to start from 
+# and choose how many iterations of starting with differing initial centroid location
 kmeans = KMeans(n_clusters=cluster_num, random_state=52,n_init=10)
 kmeans.fit(samples_array)
 
@@ -63,16 +63,40 @@ distances = kmeans.transform(samples_array)
 # Iterate over the labels we've assigned and distances associated with each sample
 # We use zip here as we're pairing these cluster labels and distances together for each sample to form a tuple
 # Since we sorted numerically in the last pipeline step, this will be accurate for sample number on the plot!
+cluster_file = open('Cluster_Info.txt', 'w')
+
 for i, (label, distance) in enumerate(zip(cluster_labels, distances)):
-    print(f"Sample {i+1}: Cluster {label+1}, Distance to centroid: {distance[label]:.4f}")
+    distance_str= f"Sample {i+1}: Cluster {label+1}, Distance to centroid: {distance[label]:.3f}"
+    print(distance_str)
+    cluster_file.write(distance_str + "\n")
 
 # Plot the results using each of the sample's associated clusters and normalised counts
 # and save it
 plt.figure(figsize=(8, 6))
-plt.scatter(range(1,len(num_list)+1), num_list, c=cluster_labels, cmap='viridis', s=100, alpha=0.8)
+scatter = plt.scatter(range(1,len(num_list)+1), num_list, c=cluster_labels, cmap='viridis', s=100, alpha=0.8)
 plt.xlabel('Sample')
 plt.ylabel('Normalised Counts')
 plt.title(f'KMeans Clustering ({cluster_num} Clusters)')
+
+# Let's create a legend so we can see which data points belong to which cluster
+# Initialise list
+legend_elements = []
+
+# Create colours for each cluster by grabbing the legend markers/data point group(s) already present
+legend_elements = scatter.legend_elements()[0]
+
+# Iterate through the unique cluster labels and assign a label 
+for i, label in enumerate(np.unique(cluster_labels)):
+    legend_elements[i].set_label(f'Cluster {label + 1}')
+
+# Show the legend and add grid
+plt.legend(handles=legend_elements, loc='upper right')
+plt.grid(True)
+
+# Show every sample on the X axis rather than every second
+plt.xticks(range(1, len(num_list) + 1))
+
+# Save figure
 plt.savefig(f'kmeans_clustering_{cluster_num}_clusters.png', bbox_inches='tight')  
 
 # Let's be safe
